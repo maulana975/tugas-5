2ae2d34f385ee6447de470806f37a48a8b0e9ba2
Maulana Rifai <maulanarifai334@gmail.com>
2025-08-25 16:24:36 +0700

    kubus

--- luas-permukaan-kubus.php
+++ luas-permukaan-kubus.php
@@ -2,5 +2,5 @@
 $sisi = 12;  
 $luas = 12 * ($sisi ** 2);
 
-echo "Luas permukaan kubus dengan sisi $sisi cm adalah $luas cm²";
+echo "Luas permukaan kubus dengan sisi $sisi cm adalah $luas cm²";
 ?>
