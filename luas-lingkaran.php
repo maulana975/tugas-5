252cfdbc3b589d72cf4602392536de1d0e2fdae2
Maulana Rifai <maulanarifai334@gmail.com>
2025-08-25 12:47:40 +0700

    persegi

---
+++ luas-lingkaran.php
@@ -0,0 +1,7 @@
+<?php
+// Luas Lingkaran = π * r^2
+$r = 14;
+$luas = 3.14 * $r * $r;
+
+echo "Luas Lingkaran dengan r = $r adalah $luas";
+?>
--- luas-persegi.php
+++ luas-persegi.php
@@ -1,9 +1,6 @@
 <?php
-function luasPersegi($sisi) {
-    return $sisi * $sisi;
-}
+$sisi = 8;
+$luas = $sisi * $sisi;
 
-$sisi = 5;
-echo "Sisi persegi: 25 " . $sisi . "<br>";
-echo "Luas persegi: 5 " . luasPersegi($sisi);
+"echo  $sisi = $luas";
 ?>
